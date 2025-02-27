26021c1188fd1894bbca573a6906b5e42befc14e
neajmorshed0 <neajmorshedimon@gmail.com>
2025-02-27 12:35:04 +0600

    update form

--- src/components/forms/FormElements/InputGroup.vue
+++ src/components/forms/FormElements/InputGroup.vue
@@ -120,7 +120,7 @@
           v-model="phoneNumber2"
           placeholder="+1 (555) 000-0000"
           type="tel"
-          class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-3 pr-[84px] text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+          class="dark:bg-dark-900 h-11 w-full p-3 rounded-lg border border-gray-300 bg-transparent py-3 pr-[84px] text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
         />
       </div>
     </div>
